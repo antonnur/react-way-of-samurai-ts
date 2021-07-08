6d586d5a5626d9ad51bc888d1d1057f12595ce81
Tont <yamalauto1@yandex.ru>
2021-07-08 07:48:53 +0500

    Pc home+

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -7,15 +7,21 @@ const MyPosts = (props: ProfilePageType) => {
   let postsElements =
     props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
+  const newPostElement: any = React.createRef()
+
+  const addPost = () => {
+    let text = newPostElement.current.values
+  }
+
   return (
     <div className={s.postsBlocks}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>
